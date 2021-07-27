828157d6dda76e8b85a697053d9977a78c491929
dparsley1203 <dparsley1203@gmail.com>
2021-07-27 16:28:21 -0500

    added option to delete results.  Verified results deleted and were able to be re-added

--- src/components/Results/ResultCard.js
+++ src/components/Results/ResultCard.js
@@ -1,12 +1,23 @@
-
-import React from "react"
+import React, { useContext } from "react"
 import  "./Result.css"
+import { useHistory } from "react-router"
+import { ResultContext } from "./ResultProvider"
 
 export const ResultCard = ({ result }) => {
 
     const combinedResults = result.benchPress + result.squat + result.deadLift + result.powerClean
     const PFP = combinedResults / result.userWeight
     const calulcated = PFP.toFixed(2)
+
+    const history = useHistory()
+    const { deleteResult } = useContext(ResultContext)
+
+    const handleDelete = () => {
+        deleteResult(result.id)
+        .then(()=> {
+            history.push("/results")
+        })
+    }
 
     return (
         <section>
@@ -45,7 +56,7 @@ export const ResultCard = ({ result }) => {
                         </tr>
                     </tbody>
                     <button className="myButton">Edit</button>
-                    <button className="myButton">Delete</button>          
+                    <button onClick={handleDelete} className="myButton">Delete</button>          
                 </table>
             </div>
         </section>
